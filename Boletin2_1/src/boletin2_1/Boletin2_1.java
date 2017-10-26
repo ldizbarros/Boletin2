2334cc39100dd336b576e37cc1472048794216fa
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2017-10-26 08:55:18 +0200

    Inicio Proyecto

---
+++ Boletin2_1/src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,11 @@
+package boletin2_1;
+
+public class Boletin2_1 {
+
+    public static void main(String[] args) {
+        float base = 4F,altura = 3F,areaT;
+        areaT = base*altura;
+        System.out.println("area triangulo = "+areaT);
+    }
+    
+}
